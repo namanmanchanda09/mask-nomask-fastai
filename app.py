5e19b2ab596a502a09c3fd95dead9d921e333a5a
Naman Manchanda <naman.mohan09@outlook.com>
2021-06-07 13:57:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from fastai.vision.all import *
+
+st.set_page_config(
+    page_title="Prediction App",
+    page_icon="🤖",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+learn = load_learner('model-2.pkl')
+
+def main():
+    st.title('Upload an image')
+    image_file = st.file_uploader('Upload a image file', ['png','jpg','jpeg'], accept_multiple_files=False)
+    if image_file is not None:
+        file_details = {"FileName":image_file.name,"FileType":image_file.type}
+        # st.write(file_details)
+        image = load_image(image_file)
+
+        img = PILImage.create(image_file)
+        # is_cat, _, probs = learn.predict(img)
+
+        pred,pred_idx,probs = learn.predict(img)
+        # print(f"Is this a cat?: {is_cat}.")
+        # print(f"Probability it's a cat: {probs[1].item():.6f}")
+        # st.write(f"Is this a cat?: {is_cat}.")
+        # st.write(f"Probability it's a cat: {probs[1].item():.6f}")
+
+        st.write(f"Prediction: {pred}; Probability: {probs[pred_idx]:.04f}")
+        
+
+
+        st.image(image)
+    
+if __name__ == main():
+    main()
